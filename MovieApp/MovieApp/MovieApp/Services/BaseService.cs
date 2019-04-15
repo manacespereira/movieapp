2be0c2fd22277b3940d300e880b26a74e9331cfe
Manacés Pereira da Silva Neto <manaces.silva@amcom.com.br>
2019-04-15 19:51:35 -0300

    restsharp installation

---
+++ MovieApp/MovieApp/MovieApp/Services/BaseService.cs
@@ -0,0 +1,17 @@
+﻿using MovieApp.Models;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieApp.Services
+{
+    public class BaseService<T> where T : class
+    {
+        protected RestClient CreateClient()
+        {
+            var client = new RestClient("https://api.themoviedb.org/3/movie");
+            return client;
+        }
+    }
+}
---
+++ MovieApp/MovieApp/MovieApp/Services/MovieService.cs
@@ -0,0 +1,18 @@
+﻿using MovieApp.Models;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieApp.Services
+{
+    public class MovieService : BaseService<Movie>
+    {
+        private readonly RestClient Client;
+
+        public MovieService()
+        {
+            Client = CreateClient();
+        }
+    }
+}

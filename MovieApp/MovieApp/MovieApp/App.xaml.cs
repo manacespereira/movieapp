43da8079a1d217dca4c87dd18c8e91092ff803fe
Manacés Pereira da Silva Neto <manaces.silva@amcom.com.br>
2019-04-15 10:03:22 -0300

    application initializing using prism as a MVVM framework

---
+++ MovieApp/MovieApp/MovieApp/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Prism;
+using Prism.Ioc;
+using MovieApp.ViewModels;
+using MovieApp.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace MovieApp
+{
+    public partial class App
+    {
+        public App() : this(null) { }
+
+        public App(IPlatformInitializer initializer) : base(initializer) { }
+
+        protected override async void OnInitialized()
+        {
+            InitializeComponent();
+
+            await NavigationService.NavigateAsync("NavigationPage/MainPage");
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<NavigationPage>();
+            containerRegistry.RegisterForNavigation<MainPage, MainPageViewModel>();
+        }
+    }
+}
---
+++ MovieApp/MovieApp/MovieApp/ViewModels/ViewModelBase.cs
@@ -0,0 +1,61 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieApp.ViewModels
+{
+    public class ViewModelBase : BindableBase, INavigationAware, IDestructible
+    {
+        protected INavigationService NavigationService { get; private set; }
+
+        public ViewModelBase(INavigationService navigationService)
+        {
+            NavigationService = navigationService;
+        }
+
+        #region Properties
+
+        private string _title;
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        private bool isBusy;
+        public bool IsBusy
+        {
+            get { return isBusy; }
+            set { SetProperty(ref isBusy, value); }
+        }
+
+        #endregion
+
+        #region Lifecycle Methods
+
+        public virtual void OnNavigatedFrom(INavigationParameters parameters)
+        {
+
+        }
+
+        public virtual void OnNavigatedTo(INavigationParameters parameters)
+        {
+
+        }
+
+        public virtual void OnNavigatingTo(INavigationParameters parameters)
+        {
+
+        }
+
+        public virtual void Destroy()
+        {
+
+        }
+
+        #endregion
+    }
+}
